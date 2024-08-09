80f5dd10c75d17b9ced674153c12656650bd9152
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-09 12:56:51 +0200

    new code is added now

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import ImgOne from './assets/adaptive-icon.png';
+import ImgOne from './assets/images/pexelslisettkruusimae.jpg';
 
 export default function App() {
   return (
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   image: {
-    width: 200,
+    width: "100%",
     height: 200,
   },
   texting: {
