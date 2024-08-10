4ae53333829ea60c3a1ed5777daf6e72572d2472
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-10 13:00:58 +0200

    new check after failing to nativeWind

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import {Text, View, Image } from 'react-native';
+import {Text, View, Image , StyleSheet} from 'react-native';
 
 export default function App() {
   return (
-    <View>
-        <Text>New design is added now</Text>
+    <View style={styles.container}>
+        <Text>New design is added now </Text>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
