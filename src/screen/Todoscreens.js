a913965cdf250822e3ddf0e0b4d8e70cc862955c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-10 15:37:35 +0200

    new design is added now

--- src/screen/Todoscreens.js
+++ src/screen/Todoscreens.js
@@ -26,16 +26,22 @@ const Todoscreens = () => {
       <View style={Global.designResult}>
         <Text style={Global.Texter}>{item.name}</Text>
         <IconButton icon="pencil" color="red" size={20} />
-        <IconButton icon="delete" color="red" size={20} />
+        <IconButton icon="delete" color="red" size={20}  onPress={() => handDeleteTodo(item.id)}/>
       </View>
-    );
+    ); 
   };
 
   // Adding a todo list,
   const handAddTodo = () => {
     setTodoList([...todolist, {id: Date.now().toString(), name: todo}]);
+    setTodo("");
   };
-
+ 
+  // Deleting a todo list,
+  const handDeleteTodo = (id) => {
+    const newTodo = todolist.filter((item) => item.id !== id);
+    setTodoList(newTodo);
+  };
   return (
     <SafeAreaView style={Global.androidSafeArea}>
       <View style={Global.AddingMargin}>
