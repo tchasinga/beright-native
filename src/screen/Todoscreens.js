7faa0331f04a8e066d4a0b0dee495874570eff79
Tchasinga jacques <tchasingajacques@gmail.com>
2024-08-10 12:27:20 +0200

    new code is added now

--- src/screen/Todoscreens.js
+++ src/screen/Todoscreens.js
@@ -1,16 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {Text, View } from 'react-native'
 import React from 'react'
 
 const Todoscreens = () => {
   return (
     <View>
-      <Text>Todoscreens pages</Text>
+      <Text className='text-2xl text-red-600 font-bold'>Todoscreens pages</Text>
     </View>
   )
 }
 
 export default Todoscreens
-
-const styles = StyleSheet.create({
-
-})
