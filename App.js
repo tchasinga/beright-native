4dfe93791a1e436ebbfe1ed49ca679ffe2a2ef76
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-15 12:54:58 +0300

    feat: Update app layout and assets, including new icons and dependencies

--- App.js
+++ App.js
@@ -4,18 +4,12 @@ import Todoscreens from './src/screen/Todoscreens';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Todoscreens />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
